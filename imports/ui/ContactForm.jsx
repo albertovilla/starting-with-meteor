fb700a2eeca504384a6aa5db8fca6e901d6d88a0
Alberto Villa <a.villa.diez@gmail.com>
2022-01-13 12:23:25 +0100

    First commit

---
+++ imports/ui/ContactForm.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import { ContactsCollection } from '../api/ContactsCollection'
+
+export const ContactForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
+
+    const saveContact = () => {
+        ContactsCollection.insert({name, email, imageUrl})
+
+        // clear the input data
+        setName("")
+        setEmail("")
+        setImageUrl("")
+    }
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input 
+                    id="name" type="text" 
+                    value={name}
+                    onChange={(e) => setName(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="email">E-mail</label>
+                <input 
+                    id="email" type="email" 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} />
+            </div>
+            <div>
+                <label htmlFor="imageUrl">Image URL</label>
+                <input 
+                    id="imageUrl" type="text" 
+                    value={imageUrl}
+                    onChange={(e) => setImageUrl(e.target.value)} />
+            </div>
+            <div>
+                <button type="button" onClick={saveContact}>
+                    Save contact
+                </button>
+            </div>
+        </form> 
+    )
+}
